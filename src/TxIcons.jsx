91d77a1a7eb22ceec7065640208d7d101d8bfb69
Rocky <wpkpda@gmail.com>
2018-04-11 16:20:01 +0600

    Quix icons support added

--- src/TxIcons.jsx
+++ src/TxIcons.jsx
@@ -24,20 +24,57 @@ export default class TxIcons extends Component {
 
   componentDidMount = () => {
     this.setState({loading: true});
-    this.props.store.httpPost({
-      plugin: PLUGIN,
-      alias: ALIAS
-    })
-      .then(({data}) => {
-        const icons = Object.values(data);
-        this.setState({icons});
+    this.loadIcons();
+    this.loadFromServer();
+  };
+
+  loadIcons = () => {
+    const cached = window.localStorage.getItem('icons');
+    const icons = cached ? JSON.parse(cached) : [];
+    this.setState(icons);
+  };
+
+  loadFromServer = () => {
+    const cached = window.localStorage.getItem('icon-hash');
+    fetch(`/index.php?option=com_quix&task=api.getIcons&${document.getElementById('jform_token').name}=1`, {credentials: 'same-origin'})
+      .then(data => data.json())
+      .then(data => {
         this.setState({loading: false});
+        const icons = data.map(icon=>{
+          const parts = icon.svg.match(/"(.*?)"/g);
+          icon['viewBox'] = parts[1].replace(/"/g, '');
+          icon['path'] = parts[2].replace(/"/g, '');
+          return icon;
+        });
+        const jsonStr = JSON.stringify(icons);
+
+        const hash = this.hashCode(jsonStr);
+        if (cached && cached === hash) {
+          return;
+        }
+
+        this.setState({icons});
+        window.localStorage.setItem('icon-hash', hash);
+        window.localStorage.setItem('icons', jsonStr);
       })
       .catch(err => {
         console.log(err);
         message.error("Failed to load icons");
         this.setState({loading: false});
-      })
+      });
+  };
+
+  hashCode = string => {
+    let hash = 0;
+    if (string.length == 0) {
+      return hash;
+    }
+    for (let i = 0; i < string.length; i++) {
+      let char = string.charCodeAt(i);
+      hash = ((hash<<5)-hash)+char;
+      hash = hash & hash;
+    }
+    return hash;
   };
 
   handleQuery = e => {
@@ -76,36 +113,31 @@ export default class TxIcons extends Component {
 
   render = () => {
     const icons = this.state.icons.filter(icon => {
-      return icon.search.terms.find(term => this.fuzzySearch(this.state.query, term));
+      return this.fuzzySearch(this.state.query, icon.name);
     });
     return (
-    <Spin spinning={this.state.loading}>
-      <div className="fm-toolbar">
-        <Input defaultValue={this.state.query} onChange={this.handleQuery} placeholder="Search icons for..."/>
-      </div>
-     
-     <div id="fm-content-holder">
-      <div id="fm-content">
-        <div className="qx-row">
-          {icons.map((icon, i) => {
-            return Object.values(icon.svg).map((svg, j) => {
-              return (<div key={`${i}-${j}`} className="fm-grid-sm" onDoubleClick={() => this.selectSVG(svg.raw)}>
-                <div className="fm-media">
-                  <div className="fm-media__thumb">
-                    <svg style={{width: 32, height: 38}} xmlns="http://www.w3.org/2000/svg"
-                          viewBox={svg.viewBox.join(' ')}>
-                      <path d={svg.path}/>
-                    </svg>
-                  </div>
-                  <div className="fm-media__caption"><span>{icon.label}</span></div>
-                </div>
-              </div>)
-            });
-          })}
+      <Spin spinning={this.state.loading}>
+        <div className="fm-toolbar">
+          <Input defaultValue={this.state.query} onChange={this.handleQuery} placeholder="Search icons for..."/>
         </div>
-      </div>
-      </div>
-    </Spin>
+
+        <div id="fm-content-holder">
+          <div id="fm-content">
+            <div className="qx-row">
+              {icons.map((icon, i) => {
+                return (<div key={`icon-${i}`} className="fm-grid-sm" onDoubleClick={() => this.selectSVG(icon.svg)}>
+                  <div className="fm-media">
+                    <div className="fm-media__thumb">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox={icon.viewBox}><path d={icon.path}/></svg>
+                    </div>
+                    <div className="fm-media__caption"><span>{icon.label}</span></div>
+                  </div>
+                </div>)
+              })}
+            </div>
+          </div>
+        </div>
+      </Spin>
     );
   };
 }
